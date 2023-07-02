9de01ad2cf0232537e57a69ba64695683b01cb59
Sivaram Yadav <nsr.learnings@gmail.com>
2023-07-02 19:15:16 +0530

    Added changes

---
+++ untitled folder/Queue.playground/Pages/Question_01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/*
+    rotate given array A rightside K times.
+ */
+
+func solutionQueue(A: [Int], K: Int) -> [Int] {
+    var result = A
+    
+    return result
+}
+
+solutionQueue(A: [1,2,3,4,5], K: 3)
+
